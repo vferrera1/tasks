62ec11ba43a10e2248fa7862c8539d7452b3a22e
vfly942 <vaflyers@gmail.com>
2022-09-20 21:42:45 -0400

    Completed tripleNumbers function

--- src/arrays.ts
+++ src/arrays.ts
@@ -13,7 +13,7 @@ export function bookEndList(numbers: number[]): number[] {
  * number has been tripled (multiplied by 3).
  */
 export function tripleNumbers(numbers: number[]): number[] {
-    return numbers;
+    return numbers.map((numeral: number): number => numeral * 3);
 }
 
 /**
