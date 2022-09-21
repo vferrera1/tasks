a35e55f101d56f63366832f9328c1ab86537568e
vfly942 <vaflyers@gmail.com>
2022-09-20 23:21:52 -0400

    Completed allRGB function

--- src/arrays.ts
+++ src/arrays.ts
@@ -78,7 +78,15 @@ export function countShortWords(words: string[]): number {
  * then return true.
  */
 export function allRGB(colors: string[]): boolean {
-    return false;
+    if (colors.length === 0) {
+        return true;
+    } else {
+        return colors.every(
+            (color: string): boolean =>
+                color === "red" || color === "blue" || color === "green"
+        );
+    }
+    //return false;
 }
 
 /**
