0c4d83c74d3d69e172e6e47435b920ec08c6c59b
vfly942 <vaflyers@gmail.com>
2022-09-20 21:37:00 -0400

    Most likely completed bookEndList function

--- src/arrays.ts
+++ src/arrays.ts
@@ -5,7 +5,7 @@
  * the number twice.
  */
 export function bookEndList(numbers: number[]): number[] {
-    return numbers;
+    return numbers.length > 0 ? [numbers[0], numbers[numbers.length - 1]] : [];
 }
 
 /**
