05ec54a1332df8a2071562367c4df40607f17d3f
vfly942 <vaflyers@gmail.com>
2022-09-22 19:24:18 -0400

    Completed addOption question

--- src/objects.ts
+++ src/objects.ts
@@ -147,7 +147,11 @@ export function duplicateQuestion(id: number, oldQuestion: Question): Question {
  * Check out the subsection about "Nested Fields" for more information.
  */
 export function addOption(question: Question, newOption: string): Question {
-    return question;
+    const expandedQuestion = {
+        ...question,
+        options: [...question.options, newOption]
+    };
+    return expandedQuestion;
 }
 
 /**
