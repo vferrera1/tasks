f7e8eb8172c469f72bbb38094767c1233ba4164c
vfly942 <vaflyers@gmail.com>
2022-09-28 22:31:22 -0400

    Completed getNames function

--- src/nested.ts
+++ src/nested.ts
@@ -52,7 +52,7 @@ export function removeQuestion(questions: Question[], id: number): Question[] {
  * questions, as an array.
  */
 export function getNames(questions: Question[]): string[] {
-    return [];
+    return questions.map((question: Question): string => question.name);
 }
 
 /***
