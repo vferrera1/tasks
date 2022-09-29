0635c15ee7086e24c1454f6fbe0700308321d581
vfly942 <vaflyers@gmail.com>
2022-09-28 23:44:58 -0400

    Completed sameType function, but is probably inefficient,

--- src/nested.ts
+++ src/nested.ts
@@ -150,7 +150,15 @@ export function publishAll(questions: Question[]): Question[] {
  * are the same type. They can be any type, as long as they are all the SAME type.
  */
 export function sameType(questions: Question[]): boolean {
-    return false;
+    const allMult: boolean = questions.every(
+        (question: Question): boolean =>
+            question.type === "multiple_choice_question"
+    );
+    const allShort: boolean = questions.every(
+        (question: Question): boolean =>
+            question.type === "short_answer_question"
+    );
+    return allMult || allShort;
 }
 
 /***
