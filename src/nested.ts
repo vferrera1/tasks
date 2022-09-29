88e6c3b8682633a3ecdc8b41f1e1b3bd8bebb9f9
vfly942 <vaflyers@gmail.com>
2022-09-28 22:38:30 -0400

    Completed sumPublishedPoints function

--- src/nested.ts
+++ src/nested.ts
@@ -70,7 +70,14 @@ export function sumPoints(questions: Question[]): number {
  * Consumes an array of questions and returns the sum total of the PUBLISHED questions.
  */
 export function sumPublishedPoints(questions: Question[]): number {
-    return 0;
+    const publishedQuestions = questions.filter(
+        (question: Question): boolean => question.published
+    );
+    return publishedQuestions.reduce(
+        (currentTotal: number, question: Question): number =>
+            currentTotal + question.points,
+        0
+    );
 }
 
 /***
