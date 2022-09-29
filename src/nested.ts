34c46342c680c7a3ef610d38ab81e340b571041e
vfly942 <vaflyers@gmail.com>
2022-09-28 22:34:18 -0400

    Completed sumPoints function

--- src/nested.ts
+++ src/nested.ts
@@ -59,7 +59,11 @@ export function getNames(questions: Question[]): string[] {
  * Consumes an array of questions and returns the sum total of all their points added together.
  */
 export function sumPoints(questions: Question[]): number {
-    return 0;
+    return questions.reduce(
+        (currentPoints: number, question: Question) =>
+            currentPoints + question.points,
+        0
+    );
 }
 
 /***
