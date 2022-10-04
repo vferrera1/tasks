3c5e5aa728fb3c569d21bc05fafb9ff3271e936a
vfly942 <vaflyers@gmail.com>
2022-10-04 17:41:35 -0400

    Completed RevealAnswer component

--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -2,5 +2,24 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function RevealAnswer(): JSX.Element {
-    return <div>Reveal Answer</div>;
+    /* Simulates a quiz application that provides a way to "reveal the answer"
+     * to a user by clicking a button. The answer will be 42 for our purposes.
+     *     You will need a state to handle whether the text is visible
+     *     There is a button labelled Reveal Answer that inverts the state
+     *     The text "42" is not initially visible
+     *     When the button is clicked, the text 42 should be visible.
+     */
+    const [visible, setVisible] = useState<boolean>(false);
+
+    /* Full flipVisibility function (non-lambda)
+     *     function flipVisibility(): void {
+     *         setVisible(!visible);
+     *     }
+     */
+    return (
+        <div>
+            <Button onClick={() => setVisible(!visible)}>Reveal Answer</Button>
+            {visible && <div>42</div>}
+        </div>
+    );
 }
