ee8f9bdbe5253ecae042c50e83ff2bbd40344a83
vfly942 <vaflyers@gmail.com>
2022-10-06 21:40:10 -0400

    Completed TwoDice component

--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -12,5 +12,54 @@ export function d6(): number {
 }
 
 export function TwoDice(): JSX.Element {
-    return <div>Two Dice</div>;
+    /* Simulates a game where you roll two dice to try to get matching values.
+     * However, you lose the game if your dice ever come up as a pair of ones ("snake eyes")
+     *     You will need two states, one for each die.
+     *     Each die's value should be rendered in the View in a <span> tag of their own,
+     * with the first die having the "data-testid" of "left-die" and the
+     * second die having the "data-testid" of "right-die".
+     *     You will need two "Roll" buttons (labelled "Roll Left" and "Roll Right")
+     *     Clicking a Roll button will change the value for the corresponding die
+     * using the provided d6 function.
+     *     The initial vlaues of the dice cannot be the same.
+     *     When the two states both equal one, render a message that includes the word Lose.
+     *     When the two states are equal (but not one), render a message that includes the word Win.
+     *     If you do all these and are still not passing all tests,
+     * read the test file, it's a good practice! In much of software engineering,
+     * the tests ARE the product spec.
+     */
+    // Two states: One for each die
+    const [leftDieValue, changeLeftValue] = useState<number>(1);
+    const [rightDieValue, changeRightValue] = useState<number>(2);
+
+    /* Add the following in as comments once you know how to do so.
+     * Dice values rendered in View in a span tag
+     * Roll buttons: When clicked, the value of the corresponding die is changed via d6 function
+     * Rendered messages upon winning (matching dice) or losing (snake eyes)
+     */
+    return (
+        <div>
+            <div>
+                <span data-testid="left-die">{leftDieValue}</span>
+                <span data-testid="right-die">{rightDieValue}</span>
+            </div>
+            <div>
+                <Button onClick={() => changeLeftValue(d6())}>Roll Left</Button>
+                <Button onClick={() => changeRightValue(d6())}>
+                    Roll Right
+                </Button>
+            </div>
+            <div>
+                {leftDieValue === rightDieValue ? (
+                    leftDieValue === 1 ? (
+                        <span>Lose</span>
+                    ) : (
+                        <span>Win</span>
+                    )
+                ) : (
+                    <span></span>
+                )}
+            </div>
+        </div>
+    );
 }
